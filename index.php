05c3f56c45e9cf37574bfc86e639486f648a2ffd
Yuto <64243257+yutoshi104@users.noreply.github.com>
2023-01-03 13:29:06 +0900

    色指定とseedを追加

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
 
-$file_list = glob('./img/*');
-$file = $file_list[(int)array_rand($file_list)];
+$color_list = [
+    "black",
+    "blue",
+    "bluepurple",
+    "brown",
+    "gray",
+    "green",
+    "leaf",
+    "lightgreen",
+    "orange",
+    "pink",
+    "purple",
+    "red",
+    "silver",
+    "skyblue",
+    "white",
+    "yellow",
+];
+
+$seed = isset($_GET['seed']) && is_numeric($_GET['seed']) ? (int)$_GET['seed'] : null;
+$color = isset($_GET['color']) && in_array($_GET['color'],$color_list) ? $_GET['color'] : null;
+
+if($color===null){
+    $file_list = glob('./img/*');
+    $file = $file_list[(int)array_rand($file_list)];
+}else{
+    $file = './img/y_'.$color.'.png';
+}
 
 $finfo = new finfo(FILEINFO_MIME_TYPE);
 $mime_type = $finfo->file($file);
